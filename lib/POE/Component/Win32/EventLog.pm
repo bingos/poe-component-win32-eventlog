7eaf01db84b1569c0374158807001d22296777ee
Chris Williams <chris@bingosnet.co.uk>
2009-04-25 22:27:37 +0100

    Updated Module::Install to 0.68

--- lib/POE/Component/Win32/EventLog.pm
+++ lib/POE/Component/Win32/EventLog.pm
@@ -16,7 +16,7 @@ use Win32::EventLog;
 use Carp qw(carp croak);
 use vars qw($VERSION);
 
-$VERSION = '1.09';
+$VERSION = '1.10';
 
 our %functions = ( qw(backup Backup read Read getoldest GetOldest getnumber GetNumber clear Clear report Report) );
 
