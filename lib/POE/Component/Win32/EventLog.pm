d73e718f19acabaa8983e89f601c3ecd77e3d859
Chris Williams <chris@bingosnet.co.uk>
2009-04-25 22:17:48 +0100

    Switched to Module::Install

--- lib/POE/Component/Win32/EventLog.pm
+++ lib/POE/Component/Win32/EventLog.pm
@@ -16,7 +16,7 @@ use Win32::EventLog;
 use Carp qw(carp croak);
 use vars qw($VERSION);
 
-$VERSION = '1.06';
+$VERSION = '1.07';
 
 our %functions = ( qw(backup Backup read Read getoldest GetOldest getnumber GetNumber clear Clear report Report) );
 
